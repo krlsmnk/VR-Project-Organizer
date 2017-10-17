1528b0ecb58dbacc6afd8c6c0e16fbc202c61368
unknown <steam_user@MOCAP-3-PC.hpc.msstate.edu>
2017-10-17 15:27:34 -0500

    caching images on machine

--- Assets/Scripts/Project/UrlItemBehavior.cs
+++ Assets/Scripts/Project/UrlItemBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 
 namespace CAVS.ProjectOrganizer.Project
 {
@@ -15,10 +15,24 @@ namespace CAVS.ProjectOrganizer.Project
 
 		private IEnumerator LoadImage(string url)
 		{
+
+            // Get Md5 hash...
+            string hashName = "something hashed";
+            
+            // Try Loading from memory...
+            string[] chunks = url.Split('.');
+            string extension = chunks[chunks.Length];
+
+
+            // Make a request then to download the source
 			WWW www = new WWW(url);
 			yield return www;
 			Renderer renderer = GetComponent<Renderer>();
 			renderer.material.mainTexture = www.texture;
+
+            // Cache the image on the machine.
+            File.WriteAllBytes(hashName + "." + extension, www.bytes);
+
 		}
 
 	}
