ae6c25042a274eb7ede2ed4e5a77df4c74d57084
EliCDavis <eli.davis1995@gmail.com>
2017-11-27 14:07:24 -0600

    Added text source

---
+++ Assets/Scripts/Project/UserInputTextSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CAVS.ProjectOrganizer.Project
+{
+	/// <summary>
+	/// This is text that was typed out manually by the user. This can happen
+	/// when creating
+	/// </summary>
+	public class UserInputTextSource : TextSource {
+
+		private string content;
+
+		public UserInputTextSource(string content)
+		{
+			this.content = content;
+		}
+
+		public override string GetContent ()
+		{
+			return this.content;
+		}
+
+		public override string Serialize ()
+		{
+			throw new NotImplementedException ();
+		}
+
+	}
+
+}
