0337c2a5e8b81e33c0cba4f8907d6bbb137be2c5
Jayzon Ty <jayzon.ty.jn8@is.naist.jp>
2018-04-04 15:36:08 -0500

    Modified Vending Machine Canvas elements

---
+++ Assets/Scripts/Scenes/VendingMachine/printFilterButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class printFilterButton : MonoBehaviour {
+
+
+	[SerializeField] private Button MyButton = null; // assign in the editor
+
+	//replace "Product" with  name of public method to run
+	void Start() { MyButton.onClick.AddListener(() => { Product();});
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
