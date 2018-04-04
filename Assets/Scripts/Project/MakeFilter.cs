5d0ffa2a45522e52bc63a98c3b42e9866b078874
Jayzon Ty <jayzon.ty.jn8@is.naist.jp>
2018-04-04 16:05:50 -0500

    Added Vending Machine model; updating MakeFilter Script

--- Assets/Scripts/Project/MakeFilter.cs
+++ Assets/Scripts/Project/MakeFilter.cs
@@ -8,36 +8,64 @@ namespace CAVS.ProjectOrganizer.Project
 
     public class MakeFilter : MonoBehaviour
     {
+		/// <summary>
+		/// make a GameObject reference to assign later
+		/// this will be used to get the text values from the vending machine
+		/// </summary>
+		public GameObject currentGameObject;
+
 
         //get the list of all items/nodes
 		Item[] nodes = ProjectFactory.BuildItemsFromCSV("CarData.csv");
 
-        //get the filter type from the scene
-            //default: numberFilter
-            string filterType = "NumberFilter";
+        //get the filter name from the scene
+			currentGameObject = GameObject.Find("Filter Name");
+			string filterName = currentGameObject.GetComponent(UI.Text).text;
+
+		//get the filter type from the scene
+			currentGameObject = GameObject.Find("Filter Type");
+			string filterType = currentGameObject.GetComponent(UI.Text).text;
 
         //get the filter value from the scene
-            //defult: 0
-            float filterValue = 0.0f;
+			currentGameObject = GameObject.Find("Filter Value");
+			string filterValue = currentGameObject.GetComponent(UI.Text).text;
+            
+		//get the field from the scene
+			currentGameObject = GameObject.Find("Field");
+			string field = currentGameObject.GetComponent(UI.Text).text;
+
+		//get the operator from the scene
+			currentGameObject = GameObject.Find("Operator");
+			string op = currentGameObject.GetComponent(UI.Text).text;
 
         //get pass/fail behavior from scene
-            //default fail case: Alpha *= 0.3
-            string behavior = "Alpha";
-            string behaviorValue = "0.3";
+			currentGameObject = GameObject.Find("Filter Behavior");
+			string behavior = currentGameObject.GetComponent(UI.Text).text;
+			currentGameObject = GameObject.Find("Behavior Value");
+			string behaviorValue = currentGameObject.GetComponent(UI.Text).text;
 
         //call filterMaker method
-        //filterMaker(nodes, filterType, filterValue, behavior, behaviorValue);
+        //filterMaker(filterName, nodes, filterType, filterValue, field, op, behavior, behaviorValue);
 
     }//end of class
 
 	/*
-    public void filterMaker(Item[] nodes, string filterType, float filterValue, string behavior, string behaviorValue)
+    public void filterMaker(string filterName, Item[] nodes, string filterType, string filterValue, string field, string op, string behavior, string behaviorValue)
     {
         //do stuff based on passed values
+        switch (filterType)
+        {
+            case "Number Filter":
+            case "String Filter":
+            case "Range Filter (Number)":
+            case "Range Filter (String)":
+		}//end of switch filterType
+
+
         //numberFilter:
-            //new NumberFilter("Year",  NumberFilter.Operator.GreaterThan, 1999)
+            //new NumberFilter(field,  NumberFilter.Operator.op, filterValue)
         //stringFilter:
-            //new StringFilter("Class", StringFilter.Operator.Equal, "Sport car")
+            //new StringFilter(field, StringFilter.Operator.op, filterValue)
 
         //create GameObject of new filter
 
--- Assets/Scripts/Scenes/VendingMachine/printFilterButton.cs
+++ Assets/Scripts/Scenes/VendingMachine/printFilterButton.cs
@@ -8,10 +8,11 @@ public class printFilterButton : MonoBehaviour {
 
 	[SerializeField] private Button MyButton = null; // assign in the editor
 
+	/*
 	//replace "Product" with  name of public method to run
 	void Start() { MyButton.onClick.AddListener(() => { Product();});
 	}
-
+	*/
 	// Update is called once per frame
 	void Update () {
 
