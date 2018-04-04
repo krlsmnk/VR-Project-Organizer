cdc477a5e62fa592effafdd4095ba100370c8f83
Jayzon Ty <jayzon.ty.jn8@is.naist.jp>
2018-04-04 13:35:25 -0500

    Updated to 2017.4, added RangeFilter scripts

---
+++ Assets/Scripts/Project/Filtering/RangeFilterNum.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CAVS.ProjectOrganizer.Project.Filtering
+{
+
+	public class RangeFilterNum : Filter
+	{
+
+		public enum Operator
+		{
+			// returns: min < x < max
+			InsideExclusive,
+			//returns: min <= x <= max
+			InsideInclusive,
+			//returns x < min OR x > max
+			Outside
+		}
+
+		/// <summary>
+		/// How we're going to perform the filter
+		/// </summary>
+		Operator op;
+
+		float min;
+		float max;
+
+		/// <summary>
+		/// The field the item might contain that we want to determine whether or not it get's pruned 
+		/// </summary>
+		protected string fieldToFilterOn;
+
+
+		public RangeFilterNum(string fieldToFilterOn, Operator op, float min, float max)
+		{
+			this.fieldToFilterOn = fieldToFilterOn;
+			this.op = op;
+			this.min = min;
+			this.max = max;
+		}
+
+		public override bool FilterItem(Item item)
+		{
+			//gets the field in question
+			string val = item.GetValue(this.fieldToFilterOn);
+			if (val == null)
+			{
+				return false;
+			}
+			//try to cast that string to a float for use in comparisons
+			float result;
+			float.TryParse(val, out result);
+
+			//actually check the result
+			return passes(result);
+		}
+
+		private bool passes(float x)
+		{
+			switch (op)
+			{
+			case Operator.InsideExclusive:
+				return min < x && x < max;
+
+			case Operator.InsideInclusive:
+				return min <= x && x <= max;
+
+			case Operator.Outside:
+				return x < min || x > max;
+			}
+			return false;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Project/MakeFilter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CAVS.ProjectOrganizer.Project
+{
+
+    public class MakeFilter : MonoBehaviour
+    {
+
+        //get the list of all items/nodes
+		Item[] nodes = ProjectFactory.BuildItemsFromCSV("CarData.csv");
+
+        //get the filter type from the scene
+            //default: numberFilter
+            string filterType = "NumberFilter";
+
+        //get the filter value from the scene
+            //defult: 0
+            float filterValue = 0.0f;
+
+        //get pass/fail behavior from scene
+            //default fail case: Alpha *= 0.3
+            string behavior = "Alpha";
+            string behaviorValue = "0.3";
+
+        //call filterMaker method
+        //filterMaker(nodes, filterType, filterValue, behavior, behaviorValue);
+
+    }//end of class
+
+	/*
+    public void filterMaker(Item[] nodes, string filterType, float filterValue, string behavior, string behaviorValue)
+    {
+        //do stuff based on passed values
+        //numberFilter:
+            //new NumberFilter("Year",  NumberFilter.Operator.GreaterThan, 1999)
+        //stringFilter:
+            //new StringFilter("Class", StringFilter.Operator.Equal, "Sport car")
+
+        //create GameObject of new filter
+
+    }
+	*/
+
+}//end of namespace
+
+
+
+
