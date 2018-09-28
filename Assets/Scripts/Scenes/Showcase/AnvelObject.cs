a89ab9cd67779bc8eef9dd579166a19f81b3b983
Marissa Hargrave <marissabhargrave@gmail.comgit config --global user.email marissabhargrave@gmail.comgit config --global user.email marissabhargrave@gmail.com>
2018-09-27 20:08:34 -0500

    Creating test scene for object creation

--- Assets/Scripts/Scenes/Showcase/AnvelObject.cs
+++ Assets/Scripts/Scenes/Showcase/AnvelObject.cs
@@ -25,7 +25,7 @@ namespace CAVS.ProjectOrganizer.Scenes.Showcase
         /// <param name="assetName"></param>
         /// <param name="parentKey">If the object does not have a parent, pass in 0</param>
 
-        public AnvelObject(AnvelControlService.Client connection, string objectName, string assetName, ObjectKey parentKey)
+        public AnvelObject(AnvelControlService.Client connection, string objectName, string assetName)
         {
             this.client = connection;
             this.objectName = objectName;
@@ -34,11 +34,19 @@ namespace CAVS.ProjectOrganizer.Scenes.Showcase
             this.objectOrientation = new Euler();
             try
             {
-                objectName = client.GetObjectDescriptorByName(objectName);
+                objectDescriptor = client.GetObjectDescriptorByName(objectName);
             }
             catch (ObjectNameNotFound e)
             {
-                objectDescriptor = client.CreateObject(assetName, objectName, parentKey);
+                objectOrientation.roll = 0;
+                objectOrientation.pitch = 0;
+                objectOrientation.yaw = 0;
+
+                objectPosition.x = 0;
+                objectPosition.y = 0;
+                objectPosition.z = 0;
+
+                objectDescriptor = client.CreateObject(assetName, objectName, 0, objectPosition, objectOrientation, false);
             }
         }
 
@@ -53,9 +61,9 @@ namespace CAVS.ProjectOrganizer.Scenes.Showcase
 
         public void SetObjectRotation(float x, float y, float z)
         {
-            objectOrientation.x = x;
-            objectOrientation.y = y;
-            objectOrientation.z = z;
+            objectOrientation.roll = x;
+            objectOrientation.pitch = y;
+            objectOrientation.yaw = z;
 
             UpdateObjectTransform();
         }
---
+++ Assets/Scripts/Scenes/Testing/ObjectCreation/ObjectCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CAVS.Anvel;
+using CAVS.Anvel.Lidar;
+using CAVS.Anvel.Vehicle;
+using AnvelApi;
+using CAVS.ProjectOrganizer.Scenes.Showcase;
+
+public class ObjectCreator : MonoBehaviour {
+
+    AnvelObject obj;
+    AnvelControlService.Client connection;
+
+    // Use this for initialization
+    void Start () {
+        AnvelControlService.Client connection = ConnectionFactory.CreateConnection();
+    }
+	
+	// Update is called once per frame
+	void OnCollision(Collider col) {
+        Debug.Log("Collision");
+        obj = new AnvelObject(connection, "newObject", "Generic 4x4");
+        obj.SetObjectPosition(0,0,0);
+    }
+}
