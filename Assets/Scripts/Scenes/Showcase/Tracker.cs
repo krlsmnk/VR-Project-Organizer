dde528333ddd78fd8191295e5873d207ce1d50d9
Eli C Davis <eli.davis1995@gmail.com>
2019-02-19 15:39:50 -0600

    fixed offset issue with tracking

--- Assets/Scripts/Scenes/Showcase/Tracker.cs
+++ Assets/Scripts/Scenes/Showcase/Tracker.cs
@@ -6,6 +6,9 @@ namespace CAVS.ProjectOrganizer.Scenes.Showcase
 {
     public class Tracker : MonoBehaviour
     {
+        [SerializeField]
+        Transform center;
+
         int v = -1;
 
         // Use this for initialization
@@ -35,20 +38,17 @@ namespace CAVS.ProjectOrganizer.Scenes.Showcase
         {
             while (true)
             {
-                var pos = transform.position;
-                UnityWebRequest www = UnityWebRequest.Get(string.Format("http://videogamedev.club:2019/set?x={0}&y={1}&z={2}&v={3}", pos.x, pos.y, pos.z, 1));
+                var offset = transform.position - center.position;
+                UnityWebRequest www = UnityWebRequest.Get(string.Format("http://videogamedev.club:2019/set?x={0}&y={1}&z={2}&v={3}", offset.x, offset.y, offset.z, v));
                 yield return www.SendWebRequest();
 
                 if (www.isNetworkError || www.isHttpError)
                 {
                     Debug.Log(www.error);
                 }
-                else
+                else if (www.downloadHandler.text != "values set")
                 {
-                    if (www.downloadHandler.text != "values set")
-                    {
-                        Debug.LogError("Unsucsessfully set values: " + www.downloadHandler.text);
-                    }
+                    Debug.LogError("Unsucsessfully set values: " + www.downloadHandler.text);
                 }
 
             }
