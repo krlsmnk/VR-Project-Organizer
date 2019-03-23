1a197db7918d694af5aa1f36b58fb353df4fe4aa
Karl Smink <krlsmnk@yahoo.com>
2019-03-23 18:12:36 -0500

    New Script doesn't work
    Not sure why "On Trigger press, run this block of code" is so fucking complicated that it needs 5+ VRTK script dependencies.

---
+++ Assets/GhostSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTK.Examples
+{ 
+public class GhostSelection : VRTK_InteractableObject
+{
+        private Transform originalItemTransform, offsetTransform;
+        private GameObject ghostClone = null;
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+                 
+            originalItemTransform = this.gameObject.transform;
+
+            //Clone the selected object, place it with some offset to the original
+            offsetTransform = originalItemTransform;
+            Vector3 newpos = offsetTransform.position;
+            newpos.x += 5.0f;
+            offsetTransform.position = newpos;
+
+            ghostClone = Instantiate(this.gameObject, offsetTransform.position, offsetTransform.rotation);            
+            
+            //Later:
+            //Place the clone next to the controller, but lock its rotation
+                //offsetTransform = controller.transform;
+
+        }
+
+        public override void StopUsing(VRTK_InteractUse usingObject)
+        {
+            base.StopUsing(usingObject);
+            
+        }
+
+        protected void Start()
+        {
+       
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            //rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+        }
+}
+
+}
---
+++ Assets/Scripts/Interation/SelectionBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace CAVS.ProjectOrganizer.Interation
+{
+	public class SelectionBehavior : MonoBehaviour, ISelectable
+	{
+        public GameObject prefab;
+        private GameObject ghostClone = null;
+		public GameObject controller;
+		private Transform offsetTransform;
+
+		public void Select(GameObject caller)
+		{
+            //Clone the selected object
+            //Place the clone next to the controller, but lock its rotation
+            offsetTransform = controller.transform;
+			Vector3 newpos = offsetTransform.position;
+            newpos.x += 5.0f;
+            offsetTransform.position = newpos;            
+
+            ghostClone = Instantiate (prefab, offsetTransform.position, this.transform.rotation, controller.transform);
+
+
+		}//end of Select()
+
+		public void UnSelect(GameObject caller)
+		{
+
+		}//end of Unselect()
+
+
+		//The ghost object should always keep the same orientation as the selected Object
+		void LateUpdate()
+		{
+			if(ghostClone!=null) ghostClone.transform.rotation = this.transform.rotation;
+		}
+	}
+
+
+
+}
