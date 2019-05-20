adf84e88e04af63aa26b229affd8b9e0c9fb0941
Karl Smink <krlsmnk@yahoo.com>
2019-05-20 14:59:11 -0500

    Begin DICanvas Integration

---
+++ Assets/Scripts/Cloneable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using VRTK;
+using VRTK.CAVS.ProjectOrganizer.Interation;
+
+namespace CAVS.ProjectOrganizer.Interation
+{    
+    public class Cloneable : MonoBehaviour, ISelectable
+    {
+        private GhostClone GhostScript;
+        private Transform originalItemTransform, offsetTransform;
+        private GameObject thisClonable;
+
+        // Use this for initialization
+        void Start()
+        {
+            GhostScript = FindObjectOfType<GhostClone>();
+            if (GhostScript == null) GhostScript = new GhostClone();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void UnSelect(UnityEngine.GameObject caller)
+        {
+            //Debug.Log("UnSelected");
+        }//end of unselect
+
+        public void SelectPress(GameObject caller)
+        {
+            //Debug.Log("Selected");
+            GhostScript.createGC(thisClonable, caller.transform);
+        }
+
+        public void SelectUnpress(GameObject caller)
+        {
+            Debug.Log("SelectUnpress");
+        }
+
+        public void Hover(GameObject caller)
+        {
+            Debug.Log("Hover");
+        }
+
+        public void UnHover(GameObject caller)
+        {
+            Debug.Log("UnHover");
+        }
+    }
+}//end of namespace
---
+++ Assets/Scripts/CustomButtonReaction.cs
@@ -0,0 +1,111 @@
+﻿    namespace VRTK.Examples
+    { 
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEventHelper;
+
+
+    public class CustomButtonReaction : MonoBehaviour {
+
+        public Button[] buttons = new Button[6];
+        private Button[] rotButtons = new Button[3], posButtons = new Button[3];
+
+        public Rigidbody cloneRigidBody;
+        private VRTK_Button_UnityEvents buttonEvents;
+
+
+        private void Start()
+        {
+            buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+            if (buttonEvents == null)
+            {
+                buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+            }
+            //buttonEvents.OnPushed.AddListener(handlePush);
+
+            TurnAll(buttons, Color.red);
+       
+            posButtons[0] = buttons[0];
+            posButtons[1] = buttons[1];
+            posButtons[2] = buttons[2];
+            rotButtons[0] = buttons[3];
+            rotButtons[1] = buttons[4];
+            rotButtons[2] = buttons[5];
+        }
+
+
+        void TurnAll(Button[] theseButtons, Color thisColor) {             
+            foreach(Button currentButton in theseButtons){ 
+               ColorBlock colors = currentButton.colors;
+               colors.normalColor = thisColor;
+               currentButton.colors = colors;
+            }
+        }
+        void TurnThis(Button thisButton, Color thisColor) {                          
+               ColorBlock colors = thisButton.colors;
+               colors.normalColor = thisColor;
+               thisButton.colors = colors;            
+        }
+
+        void ToggleThis(Button thisButton) {
+            ColorBlock colors = thisButton.colors;
+            if (colors.normalColor == Color.red) colors.normalColor = Color.green;
+            else colors.normalColor = Color.red;
+            thisButton.colors = colors;
+        }
+
+        public void translateButton() { 
+            //disable all existing constraints  
+                    cloneRigidBody.constraints = RigidbodyConstraints.None;
+                    TurnAll(buttons, Color.green);
+                    //disallow rotations
+                    cloneRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+                    TurnAll(rotButtons, Color.red); //CNG
+                    //TurnThis(buttons[3], Color.red);
+                    //TurnThis(buttons[4], Color.red);
+                    //TurnThis(buttons[5], Color.red);    
+        }
+
+        public void rotateButton() { 
+            //disable all existing constraints  
+                    cloneRigidBody.constraints = RigidbodyConstraints.None;                                                                                                                                                                                           
+                    TurnAll(buttons, Color.green);
+                    //disallow translations
+                    cloneRigidBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+                    TurnAll(posButtons, Color.red);            
+        }
+        public void posx() {         
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezePositionX;
+            ToggleThis(buttons[0]);
+        }
+        public void posy() {
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezePositionY;
+            ToggleThis(buttons[1]);
+        }
+        public void posz() {
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezePositionZ;
+            ToggleThis(buttons[2]);
+        }
+        public void rotx() {
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezeRotationX;
+            ToggleThis(buttons[3]);
+        }
+        public void roty() {
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezeRotationY;
+            ToggleThis(buttons[4]);
+        }
+        public void rotz() {
+            cloneRigidBody.constraints ^= RigidbodyConstraints.FreezeRotationZ;
+            ToggleThis(buttons[5]);
+        }
+
+        public void destroyButton() {
+            GameObject[] temporary = GameObject.FindGameObjectsWithTag("temporary");
+            foreach (GameObject thisTemp in temporary)
+            {
+                Destroy(thisTemp);
+            }
+        }
+}
+}
