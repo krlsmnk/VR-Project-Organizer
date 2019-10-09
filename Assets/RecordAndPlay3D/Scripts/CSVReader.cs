e0575b6c44756c7fe46439aa542509704b9285de
Karl Smink <krlsmnk@yahoo.com>
2019-10-09 15:15:58 -0500

    s11

---
+++ Assets/RecordAndPlay3D/Scripts/CSVReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CSVReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    
+    try
+    {
+        String st = File.ReadAllText("runtimes.csv");
+        Console.WriteLine(st + "\n");
+    }
+    catch { 
+        Console.WriteLine("Could not read file.");       
+    }
+	}//end of start
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
